a8d394957b21cbda0dbae75423cc1344fd6d1158
Gabriel Couto <juustino@live.com>
2024-05-21 22:01:22 -0300

    acertar retorno try catch e documentacao swagger q sao possiveis

--- twowatch-back/src/main/java/br/twowatch/twowatch/controller/UsuarioController.java
+++ twowatch-back/src/main/java/br/twowatch/twowatch/controller/UsuarioController.java
@@ -1,14 +1,21 @@
 package br.twowatch.twowatch.controller;
 
+import br.twowatch.twowatch.exceptions.MessagePayload;
+import br.twowatch.twowatch.exceptions.ResourceNotFoundException;
 import br.twowatch.twowatch.model.Usuario;
 import br.twowatch.twowatch.service.UsuarioService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/usuario")
@@ -43,9 +50,22 @@ public class UsuarioController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Usuario> encontraUsuarioPorId(@PathVariable int id) {
-        Usuario usuario = this.usuarioService.encontrarPorId(id);
-        return ResponseEntity.ok(usuario);
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuario encontrado",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Usuario.class))}),
+            @ApiResponse(responseCode = "404", description = "Usuario não encontrado",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Map.class))})
+    })
+    public ResponseEntity encontraUsuarioPorId(@PathVariable int id) {
+        try {
+            Usuario usuario = this.usuarioService.encontrarPorId(id);
+            return ResponseEntity.ok(usuario);
+        } catch (ResourceNotFoundException ex) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new MessagePayload("Usuario nao encontrado com esse id"));
+
+        }
     }
 
     @GetMapping
---
+++ twowatch-back/src/main/java/br/twowatch/twowatch/exceptions/MessagePayload.java
@@ -0,0 +1,10 @@
+package br.twowatch.twowatch.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class MessagePayload {
+    private String message;
+}
